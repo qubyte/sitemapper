88cf22f47b06bfd2d17b138bc4d93e084f0684b4
Mark S. Everitt <mark.s.everitt@gmail.com>
2016-09-15 21:15:21 +0100

    Renames the domain flag to start-url for clarity.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 A demo cli wrapper around [sitemap][1]. To use, run:
 
 ```bash
-go run main.go --domain <put a URL to start from here> --jobs <workers>
+go run main.go --start-url <put a URL to start from here> --jobs <workers>
 ```
 
-`--domain` is mandatory, `--jobs` defaults to `1`. `--jobs` it the number of 
-sites to allow to be requested and processed simultanously. For large sites,
-consider using this argument.
+`--start-url` is mandatory, `--jobs` defaults to `1`. `--jobs` is the number of 
+sites to allow the program to request and process simultanously. For large 
+sites, consider using this argument.
 
 [1]: https://github.com/qubyte/sitemap
--- main.go
+++ main.go
@@ -12,28 +12,28 @@ import (
 )
 
 func checkFlags() (*url.URL, int) {
-	domain := flag.String("domain", "", "A fully qualified URL for the domain to crawl.")
+	startURL := flag.String("start-url", "", "A fully qualified URL for the crawl to begin with.")
 	jobs := flag.Int("jobs", 1, "Number of simultaneous requests to allow.")
 	flag.Parse()
 
-	if *domain == "" {
-		log.Fatalln("A domain to crawl is required.")
+	if *startURL == "" {
+		log.Fatalln("A start-url to crawl is required.")
 	}
 
 	if *jobs < 1 {
 		log.Fatal("The job count cannot be less than 1.")
 	}
 
-	normalizedOriginString, err := purell.NormalizeURLString(*domain, purell.FlagsSafe)
+	normalizedOriginString, err := purell.NormalizeURLString(*startURL, purell.FlagsSafe)
 
 	if err != nil {
-		log.Fatal("Unable to normalize domain.")
+		log.Fatal("Unable to normalize start-url.")
 	}
 
 	normalizedURL, err := url.Parse(normalizedOriginString)
 
 	if err != nil || !normalizedURL.IsAbs() {
-		log.Fatal("domain must be a fully qualified URL.")
+		log.Fatal("start-url must be a fully qualified URL.")
 	}
 
 	return normalizedURL, *jobs
